b360f94c2ac0b6f0f133cbe9682966b534e67269
veena1695 <veena1695@gmail.com>
2021-08-12 12:55:34 +0530

    Add project files.

---
+++ ATMMachine/ATM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATMMachine
+{
+    public class ATM : Bank
+    {
+        
+        public sealed override void WithdrawAmount()
+        {
+            Console.WriteLine("Withdrawn from ATM ");
+        }
+        public sealed override void CheckBalance()
+        {
+            Console.WriteLine("check balance from ATM");
+
+        }
+    }
+
+    public class Transaction : ATM, IAccountInfo
+    {
+        public int TransactionID;
+        public string AccountType;
+        private ulong CardNumber;  // ulong range : 0 to 18,446,744,073,709,551,615
+                                  // credit card has 12 digits which cannot be stored in an int
+
+        // WithdrawAmount and CheckBalance cannot be overridden here as they are sealed methods
+        public void getDetails()
+        {
+            Console.WriteLine("Get All account details");
+        }
+    }
+
+    interface IAccountInfo {
+        public void getDetails();
+    }
+
+}
---
+++ ATMMachine/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ATMMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b = new Bank();
+            b.WithdrawAmount();
+            ATM a = new ATM();
+            a.WithdrawAmount();
+            Account ac = new Account();
+            ac.GetDetails();
+
+        }
+    }
+
+    //Parent Class
+    public class Bank
+    {
+        public string BankName;
+        public int Amount; // cash in bank 
+        // Encapsulation: hide unnecessary data from child class
+        private string BranchName;
+        private string IFSCCode;
+
+
+        public string getIFSCCode()
+        {
+            return IFSCCode;
+        }
+
+        public void DepositAmount() { }
+        public virtual void WithdrawAmount()
+        {
+            Console.WriteLine("Withdrawn from bank");
+        }
+        public virtual void CheckBalance()
+        {
+            Console.WriteLine("check balance from bank");
+        }
+
+    }
+
+    public sealed class Account : Bank
+    {
+          public void GetDetails()
+        {
+            Console.WriteLine("IFSC code is : {0}", getIFSCCode());
+        }
+    }
+    // Due to Account  being a sealed class it cannot be inherited by any other class
+
+
+
+}
